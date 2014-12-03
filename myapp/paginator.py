5b16cb58e6fcc013bc6291c94761ce5285530de5
httpPrincess <ryba271+git@google.com>
2014-12-03 10:08:50 +0100

    removed unused code in paginator

--- myapp/paginator.py
+++ myapp/paginator.py
@@ -19,15 +19,3 @@ class Pagination(object):
     @property
     def has_next(self):
         return self.page < self.pages
-
-    def iter_pages(self, left_edge=2, left_current=2,
-                   right_current=5, right_edge=2):
-        last = 0
-        for num in xrange(1, self.pages + 1):
-            if num <= left_edge or \
-               (self.page - left_current - 1 < num < self.page + right_current) \
-                    or num > self.pages - right_edge:
-                if last + 1 != num:
-                    yield None
-                yield num
-                last = num
