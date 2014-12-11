15070ba1a06ff9253dbfedf463c6581259346786
httpPrincess <ryba271+git@google.com>
2014-12-11 20:12:27 +0100

    mocks brought it in a wrong direction

--- tests/views_test.py
+++ tests/views_test.py
@@ -1,6 +1,4 @@
-from time import time
 import unittest
-from autho.local import LocalAuthorizer
 from myapp import app, facade, type_store, instance_store, authorizer
 from user import add_user, drop_all_users
 from mock import Mock
@@ -9,7 +7,6 @@ from mock import Mock
 class ViewsTest(unittest.TestCase):
     def setUp(self):
         self.tearDown()
-
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         self.types = ['mysql', 'eXist', 'mongo']
@@ -88,7 +85,7 @@ class ViewsTest(unittest.TestCase):
 
     def test_get_types(self):
         # no login required
-        facade.get_types = Mock(return_value=self.li)
+
         rv = self.app.get('/types/')
         for t in self.types:
             self.assertTrue(t in rv.data)
@@ -101,7 +98,7 @@ class ViewsTest(unittest.TestCase):
         self.login(self.user_list['1'])
 
         ret = {instance['id']: instance for instance in self.instances}
-        facade.get_all_instances = Mock(return_value={})
+
         rv = self.app.get('/instances/')
         self.assertEquals(rv.status_code, 200)
         self.assertTrue('No instances' in rv.data)
@@ -110,7 +107,6 @@ class ViewsTest(unittest.TestCase):
         self.assertEquals(rv.status_code, 302)
 
         self.login(self.instance_owner)
-        facade.get_all_instances = Mock(return_value=ret)
         rv = self.app.get('/instances/')
         self.assertEquals(rv.status_code, 200)
         for iid in self.instances:
@@ -140,15 +136,10 @@ class ViewsTest(unittest.TestCase):
         # able to access all its instance
 
         for i in self.instances:
-            facade.get_instance = Mock(return_value=i)
             rv = self.app.get('/instances/' + i['id'])
             self.assertEquals(rv.status_code, 200)
-            facade.get_instance.assert_called_with(
-                instance_id=i['id'],
-                uid=self.instance_owner['id'])
 
         # not existing instance for logged in user
-        facade.get_instance = Mock(return_value=None)
         rv = self.app.get('/instances/311')
         self.assertEquals(rv.status_code, 404)
         self.logout()
@@ -165,12 +156,10 @@ class ViewsTest(unittest.TestCase):
         self.login(self.user_list['1'])
 
         # logged user should access all types
-
         for t in self.types:
             rv = self.app.get('/types/' + t)
             self.assertEquals(rv.status_code, 200)
             # but must not view instances unless he has some
-            print '%r' % facade.get_instances_of_type(t, '1')
             self.assertFalse('Instances of this type' in rv.data)
 
         self.logout()
@@ -178,7 +167,7 @@ class ViewsTest(unittest.TestCase):
         self.login(self.instance_owner)
         # logged user should access all types
         ret = {instance['id']: instance for instance in self.instances}
-        facade.get_instances_of_type = Mock(return_value=ret)
+
         for t in self.types[:-1]:
             rv = self.app.get('/types/' + t)
             self.assertEquals(rv.status_code, 200)
