1a3d808c977875e7f5abea7b06e0bbba1f6179dd
httpPrincess <ryba271+git@google.com>
2015-02-04 15:33:56 +0100

    local tests helpers

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,52 @@
 __author__ = 'jj'
+
+
+def add_some_ownership(authorizer):
+    my_ownership = {
+        '1': ['0df959386fee11e4a350f0def1d0c536', '666'],
+        '2': ['828d548e6fe411e495bff0def1d0c536']
+    }
+    for uid, instance_list in my_ownership.iteritems():
+        for instance_id in instance_list:
+            authorizer.make_owner(user_id=uid, instance_id=instance_id)
+
+
+def add_some_types(type_store):
+    my_list = {
+        u'mysql': {u'description': u'mysql: world leading relational database',
+                   u'name': u'mysql',
+                   u'ts': 1416402816.064837},
+        u'neo4j': {u'description': u'native graph database',
+                   u'name': u'neo4j',
+                   u'ts': 1416487339.491318},
+        u'eXist': {u'description': u'eXist is a XML database',
+                   u'name': u'eXist',
+                   u'ts': 1416487325.564435}
+    }
+    for name, desc in my_list.iteritems():
+        type_store.update(name, desc)
+
+
+def add_some_instances(instance_store):
+    my_list = {
+        u'0df959386fee11e4a350f0def1d0c536': {
+            u'id': u'0df959386fee11e4a350f0def1d0c536',
+            u'last_info': 1416402900.545865,
+            u'status': u'running',
+            u'ts': 1416402900.542269,
+            u'type': u'mysql'},
+        u'666': {
+            u'id': u'666',
+            u'last_info': 1416398801.156127,
+            u'status': u'running',
+            u'ts': 1416398801.153139,
+            u'type': u'neo4j'},
+        u'828d548e6fe411e495bff0def1d0c536': {
+            u'id': u'828d548e6fe411e495bff0def1d0c536',
+            u'last_info': 1416398801.156127,
+            u'status': u'failed',
+            u'ts': 1416398801.153139,
+            u'type': u'mysql'}}
+
+    for k, v in my_list.iteritems():
+        instance_store.update(k, v)
--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,4 +1,5 @@
 from facade import get_facade
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask.ext.login import LoginManager
@@ -20,6 +21,34 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-facade = get_facade()
+if 'HTTP_DEBUG' in os.environ:
+    from facade.facade import Facade
+    from helpers.LocalAutho import LocalAuthorizer
+    from stores.dict_store import Store
+
+    instance_store = Store()
+    type_store = Store()
+    authorizer = LocalAuthorizer()
+
+    from helpers import add_some_types
+    add_some_types(type_store=type_store)
+
+    def send_method(routing_key, subject, message):
+        print 'Incoming message: %s %s %s' % (routing_key, subject, message)
+        if routing_key == 'info':
+            return
+        if subject == 'delete_instance':
+            instance_store.remove(message['id'])
+        else:
+            instance_store.update(message['id'], message)
+
+    facade = Facade(authorization=authorizer,
+                    type_store=type_store,
+                    instance_store=instance_store,
+                    send_method=send_method
+                    )
+
+else:
+    facade = get_facade()
 
 import myapp.views
