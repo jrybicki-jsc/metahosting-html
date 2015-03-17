9df9352fe0947ae86a9733cdbe8050adb193a957
BeneDicere <b.von.st.vieth@fz-juelich.de>
2015-03-17 14:58:09 +0100

    enable reverse proxying capabilities by using proxy headers

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -6,7 +6,27 @@ from flask.ext.login import LoginManager
 from logging import StreamHandler, DEBUG
 import sys
 
+class ReverseProxied(object):
+    """
+    Follow instructions at http://flask.pocoo.org/snippets/35/
+    to handle our reverse proxy.
+    """
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
 app.secret_key = '03aa1'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'Europe/Berlin'
 app.config.from_envvar('META-HTTP', silent=True)
